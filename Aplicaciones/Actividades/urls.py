68af86029cf408b41dfa225b18cbb9f246134a0f
steven.arequipa9883 <steven.arequipa9883@utc.edu.ec>
2024-08-05 02:10:03 -0500

    sonido

--- Aplicaciones/Actividades/urls.py
+++ Aplicaciones/Actividades/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('home', views.home, name='home'),  # Home page
     path('actividades', views.actividades, name='actividades'),
     path('aprender/', views.aprender, name='aprender'),
+    path('sonido/', views.sonido, name='sonido'),
     path('logout/', exit, name='exit'),
 ]
