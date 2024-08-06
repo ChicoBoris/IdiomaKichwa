393240d92a95c8646b592287a1eb0480b0a38ab6
btiban <boris.tiban4807@utc.edu.ec>
2024-08-05 20:54:01 -0500

    carusel

--- Aplicaciones/Actividades/urls.py
+++ Aplicaciones/Actividades/urls.py
@@ -4,6 +4,7 @@ from .views import  exit
 urlpatterns = [
     path('home', views.home, name='home'),  # Home page
     path('actividades', views.actividades, name='actividades'),
+    path('actividades/aprender/', views.aprender, name='aprender'),
     path('aprender/', views.aprender, name='aprender'),
     path('logout/', exit, name='exit'),
 ]
